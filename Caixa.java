7f1f585c214224ba0bd7a729169ba49fca8c91d4
Erick Teixeira <erickteixeiractt@gmail.com>
2026-09-23 17:02:29 -0300

    algumas correções/ continuações

--- Caixa.java
+++ Caixa.java
@@ -1,15 +1,28 @@
+import Itens.Item;
 import Pagamento.Pagamento;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Caixa {
     private Pagamento pagamento;
-    private Estoque estoque;
+    private List<Item> carrinho = new ArrayList<Item>();
 
-    public Caixa(Pagamento pagamento, Estoque estoque){
+    public void adicionarItem(Item item){
+        carrinho.add(item);
+    }
+    
+    public double calcularTotal(){
+        double total = 0;
+        for(Item item : carrinho){
+            total += item.getPreco();
+        }
+        return total;
+    }
+    
+    public void setPagamento(Pagamento pagamento){
         this.pagamento = pagamento;
-        this.estoque = estoque;
     }
-
     public void processarCompra(){
-        
-    }
+        pagamento.pagar(calcularTotal());
+    }    
 }
